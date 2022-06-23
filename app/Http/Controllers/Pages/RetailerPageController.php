ff1be414cf852b0d9865ec14a82f2abf96a7be32
abdelalim2000 <mohamed.abdelalim2000@gmail.com>
2022-06-23 17:10:55 +0200

    added alert if no retailer2

--- app/Http/Controllers/Pages/RetailerPageController.php
+++ app/Http/Controllers/Pages/RetailerPageController.php
@@ -7,12 +7,13 @@ use App\Models\City;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use RealRashid\SweetAlert\Facades\Alert;
 
 class RetailerPageController extends Controller
 {
-    public function index(): Factory|View|Application
+    public function index(): View|Factory|RedirectResponse|Application
     {
         $cities = City::query()
             ->where('active', true)
@@ -22,8 +23,9 @@ class RetailerPageController extends Controller
             })
             ->get();
 
-        if ($cities->count() == 0){
+        if ($cities->count() == 0) {
             Alert::info(trans('website.message.info'), trans('website.message.info-retailer-not-found'));
+            return back();
         }
 
         return view('site.retailer-page', compact('cities'));
